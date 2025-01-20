39cc9afd30a5330bc7ad29d1dd24c1ba130db331
Erik Behrends <behrends@users.noreply.github.com>
2025-01-20 11:04:48 +0100

    Code aufgeräumt

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-// benötigte Imports
 import { styleText } from 'node:util';
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
-
-// Definition der Funktionen
 
 // Hauptmenü
 function mainMenu() {
@@ -20,10 +17,8 @@ function mainMenu() {
     displayWeather(input, temperature);
 
     input = promptWithExit('Wie warm hättest du es gerne? ');
-    // Eingabe in Zahl konvertieren
-    let tempInput = parseInt(input);
+    let tempInput = parseInt(input); // Eingabe in Zahl konvertieren
     if (isNaN(tempInput)) {
-      // ist tempInput keine Zahl
       console.log('Ungültige Eingabe (keine Zahl):', input);
     } else {
       console.log('Deine Wunschtemperatur:', tempInput);
@@ -44,20 +39,17 @@ function promptWithExit(message) {
 
 // Ermittlung der Temperatur
 function getTemperature() {
-  let temp = Math.floor(Math.random() * 10);
-  // Ergebnis der Funktion: die Temperatur
-  return temp;
+  return Math.floor(Math.random() * 10);
 }
 
 // Ausgabe formatieren und anzeigen
 function displayWeather(input, temp) {
-  let output = styleText('blue', temp + ''); // + '' --> erzeugt String
+  let output = styleText('blue', temp + '');
   const now = new Date(); // aktueller Zeitpunkt
   const time = now.toLocaleTimeString('de-DE'); // Zeit in deutschem Format
   const weatherOutput = `Temperatur in ${input} um ${time}: ${output} Grad`;
   console.log(weatherOutput);
-  let weatherCond = temp < 5 ? 'Ziemlich kalt!' : 'Nicht zu kalt!';
-  console.log(weatherCond);
+  console.log(temp < 5 ? 'Ziemlich kalt!' : 'Nicht zu kalt!');
 }
 
 // starte Hauptmenü
