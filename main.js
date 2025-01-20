af247e0d1bbc11bc8717c964e14dc447155dfcf9
Erik Behrends <behrends@users.noreply.github.com>
2025-01-20 11:03:04 +0100

    Funktion für Hauptmenü

--- main.js
+++ main.js
@@ -5,6 +5,43 @@ const prompt = promptSync();
 
 // Definition der Funktionen
 
+// Hauptmenü
+function mainMenu() {
+  let output = styleText(
+    'cyan',
+    "Das Programm kann mit der Eingabe 'x' beendet werden."
+  );
+  console.log(output);
+  while (true) {
+    let input = promptWithExit(
+      'Für welche Stadt willst du das Wetter wissen? '
+    );
+    let temperature = getTemperature();
+    displayWeather(input, temperature);
+
+    input = promptWithExit('Wie warm hättest du es gerne? ');
+    // Eingabe in Zahl konvertieren
+    let tempInput = parseInt(input);
+    if (isNaN(tempInput)) {
+      // ist tempInput keine Zahl
+      console.log('Ungültige Eingabe (keine Zahl):', input);
+    } else {
+      console.log('Deine Wunschtemperatur:', tempInput);
+    }
+  }
+}
+
+// Eingabe mit Prompt und Test, ob Programm beendet werden soll
+function promptWithExit(message) {
+  const input = prompt(message);
+  if (input === 'x') {
+    let output = styleText('cyan', 'Auf Wiedersehen!');
+    console.log(output);
+    process.exit(0);
+  }
+  return input;
+}
+
 // Ermittlung der Temperatur
 function getTemperature() {
   let temp = Math.floor(Math.random() * 10);
@@ -23,27 +60,6 @@ function displayWeather(input, temp) {
   console.log(weatherCond);
 }
 
+// starte Hauptmenü
 console.log('Willkommen zur Wetter-App!');
-
-// Hauptprogramm
-while (true) {
-  let input = prompt(
-    'Für welche Stadt willst du das Wetter wissen? '
-  );
-  if (input === 'x') {
-    process.exit(0);
-  }
-  let temperature = getTemperature();
-  displayWeather(input, temperature);
-
-  input = prompt('Wie warm hättest du es gerne? ');
-  // Eingabe in Zahl konvertieren
-  let tempInput = parseInt(input);
-  if (isNaN(tempInput)) {
-    // ist tempInput keine Zahl
-    console.log('Ungültige Eingabe (keine Zahl):', input);
-  } else {
-    console.log('Deine Wunschtemperatur:', tempInput);
-  }
-}
-
+mainMenu();
